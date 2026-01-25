acf3f04fa1c33cf05ffbbfa4ebeca8a4dd37986f
Jose luis <jyana22@gmail.com>
2026-01-24 23:36:31 -0500

    feat: implement

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+// tailwind.config.mjs
+export default {
+	theme: {
+		extend: {
+			colors: {
+				primary: '#2499D6', // El azul de tus botones
+				secondary: '#0F172A', // El oscuro del footer
+			}
+		}
+	}
+}
